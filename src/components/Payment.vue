9e1ccfc9ac01f3f58d0386f67f87237acfbe1fd6
watasho358 <t8mmmxc11y@yahoo.co.jp>
2022-09-04 20:16:16 +0900

    v-on 処理を埋め込む

--- src/components/Payment.vue
+++ src/components/Payment.vue
@@ -6,6 +6,10 @@ const price1 = 40000
 const price2 = 20000
 
 const url1 = 'https://www.amazon.co.jp/YEARCOLOR-%E3%82%B7%E3%83%B3%E3%83%97%E3%83%AB%E3%83%AF%E3%83%BC%E3%82%AF%E3%83%87%E3%82%B9%E3%82%AF-%E3%82%B2%E3%83%BC%E3%83%9F%E3%83%B3%E3%82%B0%E3%83%86%E3%83%BC%E3%83%96%E3%83%AB-%E8%80%90%E8%8D%B7%E9%87%8D135KG-%E3%81%97%E3%81%A3%E3%81%8B%E3%82%8A%E3%81%97%E3%81%9F%E6%A7%8B%E9%80%A0/dp/B0B7667976/?_encoding=UTF8&pd_rd_w=guOEN&content-id=amzn1.sym.79ec8dbb-a6ed-4b2c-b8a2-e2dc98848cba&pf_rd_p=79ec8dbb-a6ed-4b2c-b8a2-e2dc98848cba&pf_rd_r=BS3RH2WYYAWDQ1Y6X0C5&pd_rd_wg=Womz2&pd_rd_r=3bf35fc8-ba80-44d1-8c8c-134dc3929d04&ref_=pd_gw_ci_mcx_mr_hp_atf_m'
+
+const buy = (itemName: string) => {
+  alert('Are you dure to buy ' + itemName + '?')
+}
 </script>
 
 <template>
@@ -14,11 +18,15 @@ const url1 = 'https://www.amazon.co.jp/YEARCOLOR-%E3%82%B7%E3%83%B3%E3%83%97%E3%
     <div class="payment">
       <label>{{ itemName1 }}</label>
       <label>{{ price1 }} yen</label>
+      <!-- v-bind:href -->
       <a :href=url1>bought at...</a>
+      <!-- v-on:click -->
+      <button @click="buy(itemName1)">BUY</button>
     </div>
     <div class="payment">
       <label>{{ itemName2 }}</label>
       <label>{{ price2 }} yen</label>
+      <button @click="buy(itemName2)">BUY</button>
     </div>
   </div>
 </template>
